88b0524a0d4033cfb8269dadb43e6f7dd848f762
Soham Darak <165137756+Soham-Darak@users.noreply.github.com>
2026-06-08 16:22:22 +0530

    Time: 3 ms (98.90%) | Memory: 49.2 MB (74.61%) - LeetSync

---
+++ 4266-angles-of-a-triangle/angles-of-a-triangle.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double[] internalAngles(int[] sides) {
+        double a = sides[0], b = sides[1], c = sides[2];
+        if((a+b)<=c || (b+c)<=a || (c+a)<=b){
+            return new double[0];
+        }
+        double A = Math.toDegrees(Math.acos((b*b + c*c - a*a)/(2*b*c)));
+        double B = Math.toDegrees(Math.acos((a*a + c*c - b*b)/(2*a*c)));
+        double C = 180 - A - B;
+        double[] angles = {A, B, C};
+        Arrays.sort(angles);
+
+        return angles;
+    }
+}
