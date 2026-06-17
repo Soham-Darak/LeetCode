2ba6b414f932847a9374b9640e00fcd9100a564d
Soham Darak <165137756+Soham-Darak@users.noreply.github.com>
2026-06-17 17:38:01 +0530

    Time: 2 ms (95.14%) | Memory: 47.7 MB (50.66%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n+1];
+        int[] result = new int[2];
+
+        for(int num : nums){
+            freq[num]++;
+        }
+
+        for(int i = 0; i <= n;i++){
+            if(freq[i] == 2){
+                result[0] = i;
+            }
+            else if(freq[i] == 0){
+                result[1] = i;
+            }
+        }
+        return result;
+    }
+}
