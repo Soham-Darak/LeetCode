01911cc4b276bdaca82882bd110b7e5403184ed6
Soham Darak <165137756+Soham-Darak@users.noreply.github.com>
2026-07-03 16:28:03 +0530

    Time: 11 ms (32.46%) | Memory: 48.1 MB (11.67%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -1,26 +1,28 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
-        int min1 = Integer.MAX_VALUE;
-        int min2 = Integer.MAX_VALUE;
+       Arrays.sort(nums);
+       return ((nums[nums.length-1]*nums[nums.length-2])-(nums[0]*nums[1]));
+        // int min1 = Integer.MAX_VALUE;
+        // int min2 = Integer.MAX_VALUE;
 
-        int max1 = Integer.MIN_VALUE;
-        int max2 = Integer.MIN_VALUE;
+        // int max1 = Integer.MIN_VALUE;
+        // int max2 = Integer.MIN_VALUE;
 
-        for(int num: nums){
-            if(num < min1){
-                min2 = min1;
-                min1 = num;
-            }else if(num < min2){
-                min2 = num;
-            }
+        // for(int num: nums){
+        //     if(num < min1){
+        //         min2 = min1;
+        //         min1 = num;
+        //     }else if(num < min2){
+        //         min2 = num;
+        //     }
 
-            if(num > max1){
-                max2 = max1;
-                max1 = num;
-            }else if(num > max2){
-                max2 = num;
-            }
-        }
-        return ((max1 * max2) - (min1 * min2));
+        //     if(num > max1){
+        //         max2 = max1;
+        //         max1 = num;
+        //     }else if(num > max2){
+        //         max2 = num;
+        //     }
+        // }
+        // return ((max1 * max2) - (min1 * min2));
     }
 }
